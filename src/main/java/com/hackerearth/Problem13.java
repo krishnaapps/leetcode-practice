a06cd3aa30d20aa67028322a5a68b484c6615ebb
KRISHNA <chaitu.msit@gmail.com>
2025-10-21 16:39:01 +0530

    Adding Problems

---
+++ src/main/java/com/hackerearth/Problem13.java
@@ -0,0 +1,41 @@
+package com.hackerearth;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem13 {
+
+    public static void main(String[] args){
+        int result = Problem13.birthday(Arrays.asList(new Integer[]{2, 2, 1, 3, 2}),4,2);
+        System.out.println(result);
+
+        int result1 = Problem13.birthday(Arrays.asList(new Integer[]{1, 2, 1, 3, 2}),3,2);
+        System.out.println(result1);
+
+        int result2 = Problem13.birthday(Arrays.asList(new Integer[]{1, 1, 1, 1, 1,1}),3,2);
+        System.out.println(result2);
+    }
+
+    public static int birthday(List<Integer> s, int d, int m) {
+        // Write your code here
+        int result = 0;
+        int start = 0;
+        int first = 0;
+        int end = first+m;
+        while(end<=s.size()-1){
+            int currentSum = 0;
+            while(start<end){
+                currentSum+=s.get(start);
+                start++;
+            }
+            if(currentSum==d){
+                result++;
+            }
+            first++;
+            start = first;
+            end = first+m;
+
+        }
+        return result;
+    }
+}
